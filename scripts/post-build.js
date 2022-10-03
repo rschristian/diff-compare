07b54d18c70f1c7bd0b9b7fd15a5ff77bd56c873
Ryan Christian <rchristian@ryanchristian.dev>
2022-10-03 04:12:27 -0500

    refactor: Update post-build script

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,15 +1,22 @@
 import { promises as fs } from 'node:fs';
 
-(async function inlineCSS() {
-    const linkRegex = /<link rel="stylesheet" href="\/assets\/styles([^\n]*)/;
-    const css = await fs.readFile(
-        `dist/assets/styles/${(await fs.readdir('dist/assets/styles'))[0]}`,
-        'utf-8',
-    );
+let html = await fs.readFile('dist/index.html', 'utf-8');
 
-    let html = await fs.readFile('dist/index.html', 'utf-8');
-    if (linkRegex.test(html)) {
-        html = html.replace(linkRegex, `<style>${css}</style>`);
-        await fs.writeFile('dist/index.html', html);
-    }
-})();
+// Scripts
+const themeSwitcherJS = await fs.readFile(
+    `dist/${(await fs.readdir('dist')).find((entry) => /^themeSwitcher/.test(entry))}`,
+    'utf-8',
+);
+html = html.replace(
+    /<script.*\/themeSwitcher[^\n]*/g,
+    `<script type="module">${themeSwitcherJS}</script>`,
+);
+
+// CSS
+const globalCSS = await fs.readFile(
+    `dist/assets/styles/${(await fs.readdir('dist/assets/styles'))[0]}`,
+    'utf-8',
+);
+html = html.replace(/<link.*href=".*global[^\n]*/, `<style>${globalCSS}</style>`);
+
+await fs.writeFile('dist/index.html', html);
