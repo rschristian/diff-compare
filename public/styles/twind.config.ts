8b050333f1eba6bf66f380518b666d8d6fdb4dd7
Ryan Christian <rchristian@ryanchristian.dev>
2023-01-03 12:22:59 -0600

    chore: Updating twind ignorelist

--- public/styles/twind.config.ts
+++ public/styles/twind.config.ts
@@ -5,6 +5,7 @@ export const twindConfig = defineConfig({
     darkMode: 'class',
     presets: [presetTailwind()],
     hash: false,
+    ignorelist: ['dark', 'diff', 'removal', 'addition'],
     theme: {
         colors: {
             primary: {
