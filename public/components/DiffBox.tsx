2c507e75a820064d837d7150bef26b451f14b6fc
Ryan Christian <rchristian@ryanchristian.dev>
2023-12-18 03:55:51 -0600

    feat: Don't diff identical inputs

--- public/components/DiffBox.tsx
+++ public/components/DiffBox.tsx
@@ -7,18 +7,27 @@ interface Props {
 
 export function DiffBox({ diffedParts }: Props) {
     return (
-        <pre class="h-96 w-full p-2 bg-code(& dark:dark) overflow-auto rounded shadow-md">
-            {diffedParts.pending.value
-                ? ''
-                : diffedParts.value.map((part) => (
-                      <span
-                          class={`${
-                              part.added ? 'diff addition' : part.removed ? 'diff removal' : ''
-                          }`}
-                      >
-                          {part.value}
-                      </span>
-                  ))}
-        </pre>
+        diffedParts.pending.value
+            ? <div class="h-96"><span class="loader"></span></div>
+            : <pre
+                class={`${
+                    diffedParts.value.length > 1
+                        ? 'w-full bg-code(& dark:dark)'
+                        : 'h-auto bg-primary-dark'
+                } h-96 p-2 overflow-auto rounded shadow-md`}
+            >
+                {diffedParts.value.length === 1
+                    ? <span class="bg-primary-dark">The two inputs are identical</span>
+                    : diffedParts.value.map((part) => (
+                        <span
+                            class={`${
+                                part.added ? 'diff addition' : part.removed ? 'diff removal' : ''
+                            }`}
+                        >
+                            {part.value}
+                        </span>
+                    ))
+                }
+            </pre>
     );
 }
