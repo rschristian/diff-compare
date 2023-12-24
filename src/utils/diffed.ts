2b0ba28d1acceddadeda741b661a75e8ef1ef636
Ryan Christian <rchristian@ryanchristian.dev>
2023-12-24 01:22:08 -0600

    fix: Prerendering of diff worker

--- src/utils/diffed.ts
+++ src/utils/diffed.ts
@@ -14,6 +14,7 @@ export function diffed(
     receivedFormatted: AsyncComputedSignalPayload<string>,
 ) {
     return asyncComputed<DiffPart[]>(async () => {
+        if (typeof window === 'undefined') return []
         while (!expectedFormatted.value || !receivedFormatted.value) {
             await new Promise((r) => setTimeout(r, 5));
         }
